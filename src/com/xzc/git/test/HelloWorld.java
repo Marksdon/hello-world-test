04789918c2b7e5f1d61a93fcb79c8dede7450fe9
Marksdon <412176694@qq.com>
2016-09-01 00:20:44 +0800

    test for the second time

--- src/com/xzc/git/test/HelloWorld.java
+++ src/com/xzc/git/test/HelloWorld.java
@@ -3,5 +3,6 @@ package com.xzc.git.test;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hellow git");
+		System.out.println("This is my second time");
 	}
 }
